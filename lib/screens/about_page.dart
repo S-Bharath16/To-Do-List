d0568d8402e41d7f0420e0c801ad96353a18752c
lucifer-b16 <bharathshan@outlook.com>
2024-08-04 01:03:20 +0530

    Final commit for V1

--- lib/screens/about_page.dart
+++ lib/screens/about_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class AboutPage extends StatelessWidget {
   final String _avatarUrl = 'assets/images/avatar.jpeg';
@@ -7,17 +6,10 @@ class AboutPage extends StatelessWidget {
   final String _linkedinUrl = 'https://www.linkedin.com/in/bharathssss/';
   final String _instagramUrl = 'https://www.instagram.com/bharath_.16._/';
 
-  Future<void> _launchUrl(String url) async {
-    final Uri uri = Uri.parse(url);
-    if (await canLaunchUrl(uri)) {
-      await launchUrl(uri, mode: LaunchMode.externalApplication);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('About'),
@@ -35,38 +27,51 @@ class AboutPage extends StatelessWidget {
             SizedBox(height: 20),
             Text(
               'S Bharath',
-              style: Theme.of(context).textTheme.headlineSmall,
+              style: textTheme.headlineSmall,
             ),
             Text(
               'Flutter Developer',
-              style: Theme.of(context).textTheme.titleMedium,
+              style: textTheme.titleMedium,
             ),
             SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                _buildIconButton('assets/icons/github.png', _githubUrl),
+                _buildIcon(context, 'assets/icons/github.png', 'GitHub'),
                 SizedBox(width: 20),
-                _buildIconButton('assets/icons/linkedin.png', _linkedinUrl),
+                _buildIcon(context, 'assets/icons/linkedin.png', 'LinkedIn'),
                 SizedBox(width: 20),
-                _buildIconButton('assets/icons/instagram.png', _instagramUrl),
+                _buildIcon(context, 'assets/icons/instagram.png', 'Instagram'),
               ],
+            ),
+            SizedBox(height: 10),
+            Text(
+              'GitHub: S-Bharath16',
+              style: textTheme.bodyMedium,
+            ),
+            Text(
+              'LinkedIn: bharathssss',
+              style: textTheme.bodyMedium,
+            ),
+            Text(
+              'Instagram: bharath_.16._',
+              style: textTheme.bodyMedium,
             ),
             Spacer(),
             Text(
               'Version 1.0.0',
-              style: Theme.of(context).textTheme.bodyMedium,
+              style: textTheme.bodyMedium,
             ),
             SizedBox(height: 10),
             Text(
               'This app helps you manage your tasks efficiently. You can add, update, and delete tasks. The app also provides notifications to remind you of your tasks.',
               textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.bodyMedium,
+              style: textTheme.bodyMedium,
             ),
             SizedBox(height: 20),
             Text(
               'Developed by Bharath',
-              style: Theme.of(context).textTheme.bodyMedium,
+              style: textTheme.bodyMedium,
             ),
             SizedBox(height: 20),
           ],
@@ -75,24 +80,32 @@ class AboutPage extends StatelessWidget {
     );
   }
 
-  Widget _buildIconButton(String assetPath, String url) {
-    return GestureDetector(
-      onTap: () => _launchUrl(url),
-      child: Container(
-        padding: EdgeInsets.all(8),
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          border: Border.all(
-            color: Colors.grey,
-            width: 1.0,
+  Widget _buildIcon(BuildContext context, String assetPath, String id) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: Colors.grey,
+              width: 1.0,
+            ),
+          ),
+          child: Image.asset(
+            assetPath,
+            width: 40,
+            height: 40,
           ),
         ),
-        child: Image.asset(
-          assetPath,
-          width: 40,
-          height: 40,
+        SizedBox(height: 5),
+        Text(
+          id,
+          style: textTheme.bodyMedium,
         ),
-      ),
+      ],
     );
   }
 }
